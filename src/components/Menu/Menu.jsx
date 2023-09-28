d33f1a29d8c6c02396e2bb6eaa7177034afd1a35
thtshajjad <shagors@users.noreply.github.com>
2023-09-28 14:17:34 +0600

    make reusable components for list of blog and side menu reusable compoents make and check

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 import styles from "./menu.module.css";
-import Link from "next/link";
-import Image from "next/image";
+import MenuPosts from "../menuPosts/menuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <div className={styles.items}>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>Anabia</span>
-              <span className={styles.date}>26.10.2023</span>
-            </div>
-          </div>
-        </Link>
-      </div>
+      <MenuPosts withImage={false} />
+
+      <h2 className={styles.subtitle}>Discover the topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
     </div>
   );
 };
