4ef84ed73c82b18709b52babcbcd19a406acbf77
thtshajjad <shagors@users.noreply.github.com>
2023-09-28 12:30:32 +0600

    card list menu update and design

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Anabia</span>
+              <span className={styles.date}>26.10.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import styles from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Card = () => {
-  return <div className={styles.container}>Card</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.details}>
+          <span className={styles.date}>24.04.2023 - </span>
+          <span className={styles.category}>Culture</span>
+        </div>
+        <Link href="/">
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut natus
+          vitae excepturi, soluta delectus eaque quisquam reiciendis quae
+          commodi ab quia itaque incidunt maxime consequatur quo beatae maiores
+          harum distinctio....
+        </p>
+        <Link href="/" className={styles.link}>
+          Read more
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
