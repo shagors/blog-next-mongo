ac6c80e046b08ccfb1d872a2f335fb4245b65bfe
thtshajjad <shagors@users.noreply.github.com>
2023-09-28 15:36:56 +0600

    add some feture and design footer page and make all pages responsive and check it

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image
+            src="/logo.png"
+            alt="Shajjad blog"
+            width={50}
+            height={50}
+            className={styles.image}
+          />
+          <h1 className={styles.logoText}>shajjadblog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit delectus
+          non atque nihil magnam quod rem cumque ex, voluptatibus repudiandae
+          nisi corporis, quis laborum reprehenderit eaque animi, ea incidunt
+          est.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+          <Image src="/instagram.png" alt="facebook" width={18} height={18} />
+          <Image src="/tiktok.png" alt="facebook" width={18} height={18} />
+          <Image src="/youtube.png" alt="facebook" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Home</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Food</Link>
+          <Link href="/">Conding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./pagination.module.css";
 
 const Pagination = () => {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.button}>Prevoius</div>
+      <div className={styles.button}>Next</div>
+    </div>
+  );
 };
 
 export default Pagination;
